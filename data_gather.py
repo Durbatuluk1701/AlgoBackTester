ac4618f6f14d16d73deb47b8d5248f0ae6387259
Will Thomas <30wthomas@gmail.com>
2022-04-08 21:01:12 -0500

    Data Gatherer upgrades

--- data_gather.py
+++ data_gather.py
@@ -1,4 +1,22 @@
 import time
+import http.client as hc
 
-def gather_data(ticker, startDate = 0, endDate = (time.time() // 1)):
-    print("test")
+baseUrl = "query1.finance.yahoo.com"
+
+"""
+Gathers data from a URL and saves it out
+:param 'ticker': the ticker to query for
+:param 'startDate': the start date as a unix epoch time
+:param 'endDate': the end date as a unix epoch time
+"""
+def gather_data(ticker : str, startDate = 0, endDate = ((int)(time.time() // 1))):
+    ticker = ticker.upper()
+    connection = hc.HTTPSConnection(baseUrl)
+    url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={startDate}&period2={endDate}&interval=1d&events=history&includeAdjustedClose=true"
+    print(url)
+    connection.request("GET", url)
+    resp = connection.getresponse()
+    print(resp)
+    with open(f"_data/{ticker}.csv","wb") as outFile:
+        outFile.write(resp.read())
+    print("Done")
