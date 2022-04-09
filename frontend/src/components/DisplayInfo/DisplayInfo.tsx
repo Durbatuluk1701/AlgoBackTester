2c13e243960fcb2fc8ae3cccdabecd438f11cecc
Will Thomas <30wthomas@gmail.com>
2022-04-09 16:59:30 -0500

    Date range picking functionality

--- frontend/src/components/DisplayInfo/DisplayInfo.tsx
+++ frontend/src/components/DisplayInfo/DisplayInfo.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { DataGrid, GridColDef } from "@mui/x-data-grid"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+import TextField from "@mui/material/TextField"
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { Button } from "@mui/material";
 
 interface ApiData {
     index: number;
@@ -13,20 +18,51 @@ interface ApiData {
     high: number;
 }
 
+const NullTicker = () => {
+    return (
+        <>
+        <h2>Error</h2>
+        <p>You have not specified a ticker to collect data for</p>
+        </>
+    )
+}
+
 export const DisplayInfo = () => {
     let [searchParams, setSearchParams] = useSearchParams();
     let [displayData, setDisplayData] = useState<Array<ApiData>>([]);
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+
 
     let ticker = searchParams.get("ticker");
-    let start = searchParams.get("start")
-    let end = searchParams.get("end")
+    let startParam = searchParams.get("start")
+    let endParam = searchParams.get("end")
 
+    let searchStr = `http://127.0.0.1:5000/${ticker}/all` + ((startParam || endParam) ? `?` : "") + (startParam ? `start=${startParam}` : "") + ((startParam && endParam) ? `&end=${endParam}` : (endParam ? `end=${endParam}` : ""))
+
+    // Update start
     useEffect(() => {
+        if (startParam) {
+            setStart(startParam);
+        }
+    }, [startParam])
+
+    // Update end
+    useEffect(() => {
+        if (endParam) {
+            setEnd(endParam);
+        }
+    }, [endParam]);
+
+    // Retrieve ticker info side effect
+    useEffect(() => {
+        if (!ticker) {
+            // Null ticker bad
+            return;
+        }
         let abortController = new AbortController();
 
         async function get_data() {
-            let searchStr = `http://127.0.0.1:5000/${ticker}/all` + ((start || end) ? `?` : "") + (start ? `start=${start}` : "") + ((start && end) ? `&end=${end}` : (end ? `end=${end}` : ""))
-            console.log(searchStr)
             let response = await fetch(searchStr, {
                 signal: abortController.signal,
             });
@@ -41,8 +77,9 @@ export const DisplayInfo = () => {
         return () => {
             abortController.abort();
         };
-    }, [ticker, end, start]);
+    }, [searchStr, ticker]);
 
+    // Grid column definition
     const columns: GridColDef[] = [
         {
             field: 'date', headerName: 'Date', width: 250, valueFormatter: (val) => {
@@ -52,16 +89,47 @@ export const DisplayInfo = () => {
                 return d.toDateString()
             }
         },
-        { field: 'open',headerName: 'Open',width: 150}, 
+        { field: 'open', headerName: 'Open', width: 150 },
         { field: 'close', headerName: "Close", width: 150 },
         { field: 'high', headerName: "High", width: 150 },
         { field: 'low', headerName: "Low", width: 150 },
         { field: 'volume', headerName: "Volume", width: 150 },
-        ]
+    ]
 
-    return (
+    return (ticker == null) ? <NullTicker /> : (
         <div style={{ display: "flex", height: "80%", width: "auto", flexDirection: "column" }}>
-            <h2 style={{margin: "0 auto"}}>{ticker} Historical Data</h2>
+            <h2 style={{ margin: "0 auto" }}>{ticker} Historical Data</h2>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                        label="Start Date"
+                        value={new Date(start)}
+                        onChange={(val) => {
+                            if (val) {
+                                try {
+                                    setStart(`${val.getUTCFullYear()}-${val.getUTCMonth() + 1}-${val.getUTCDate()}`)
+                                } catch (e) {
+
+                                }
+                            }
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                    <DatePicker
+                        label="End Date"
+                        value={new Date(end)}
+                        onChange={(val) => {
+                            if (val) {
+                                try {
+                                    setEnd(`${val.getUTCFullYear()}-${val.getUTCMonth() + 1}-${val.getUTCDate()}`)
+                                } catch { }
+                            }
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+                <Button onClick={() => setSearchParams({ ticker: (ticker ? ticker : ""), start: start, end: end})}>Refresh Data</Button>
+            </div>
             <DataGrid columns={columns} rows={displayData} getRowId={(row) => {
                 return row.index
             }} />
